b8e152f385eb4e3c97dccc0dfff514ad37513112
yutakaba <lntdk1121@gmail.com>
2025-06-08 17:34:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# codeshock-ui
---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+import { styled } from '@linaria/react';
+
+type Props = {
+  children: React.ReactNode;
+  href?: string;
+};
+
+export const Button = ({ children, href }: Props) => {
+  return <StyledButton href={href}>{children}</StyledButton>;
+};
+
+const StyledButton = styled.a`
+  background-color: #0070f3;
+  color: white;
+  padding: 12px 24px;
+  border-radius: 8px;
+  text-decoration: none;
+  font-weight: bold;
+  transition: background 0.2s;
+
+  &:hover {
+    background-color: #005ec4;
+  }
+`;
